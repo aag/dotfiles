abdeb3768abaf342a5f07a70190ae239073c6587
Adam Goforth <code@adamgoforth.com>
2018-07-19 22:24:10 +0200

    Add better Vundle update support

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-git submodule update # Update Vundle
+# Install or update Vundle
+git submodule init
+git submodule sync
+git submodule update
+
+# Set up vim dotfiles
 stow -R -v -t ~/ vim
+
+# Install or update all vim plugins using Vundle
 vim -n -c "set nomore" -c ":PluginInstall!" -c ":q" -c ":q" -c ":q"
